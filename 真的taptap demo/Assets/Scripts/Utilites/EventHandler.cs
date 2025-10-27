67572a915b6cf992a419a4a5688f0f12454b411a
Erin Yang <asabust@users.noreply.github.com>
2025-10-27 11:26:08 +0900

    增加工具类

---
+++ 真的taptap demo/Assets/Scripts/Utilites/EventHandler.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventHandler
+{
+    public static event Action<ItemDetails> UpdateItemDetails;
+
+    public static void CallUpdateItemDetails(ItemDetails details)
+    {
+        UpdateItemDetails?.Invoke(details);
+    }
+    
+    public static event Action BeforeSceneUnloadEvent;
+    public static void CallBeforeSceneUnloadEvent()
+    {
+        BeforeSceneUnloadEvent?.Invoke();
+    }
+    
+    public static event Action AfterSceneLoadEvent;
+    public static void CallAfterSceneLoadEvent()
+    {
+        AfterSceneLoadEvent?.Invoke();
+    }
+}
